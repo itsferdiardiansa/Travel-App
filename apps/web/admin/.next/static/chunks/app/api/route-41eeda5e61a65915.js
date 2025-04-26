c39a153e8c80b14dfd93b43b1c33732d02a5bc5b
Ferdi Ardiansa <ferdiardiansa@gmail.com>
2025-04-27 00:09:10 +0700

    refactor: removed cachefiles

--- apps/web/admin/.next/static/chunks/app/api/route-41eeda5e61a65915.js
+++
@@ -1,8 +0,0 @@
-;(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
-  [843],
-  { 3629: () => {} },
-  _ => {
-    var e = e => _((_.s = e))
-    _.O(0, [699, 224, 358], () => e(3629)), (_N_E = _.O())
-  },
-])
